5de3318268aa46dcde42261bf8d4cdd3cf444901
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2026-01-16 23:17:48 +0530

    Added Home Page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar.jsx'
+export default function Home(){
+    return(
+        <section className="home">
+            <Navbar />
+            <h1>Welcome to Inventory management app!</h1>
+        </section>
+
+    )
+}
