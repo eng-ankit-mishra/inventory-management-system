313bcd45ae8cee093c8495fdf03b4ab7ce970c1f
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2026-01-17 23:16:19 +0530

    added some whitspacing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
+import Button from './Button.jsx'
 export default function Navbar(){
     return (
         <nav className="navbar">
-            <h2>InventoryApp</h2>
-            <button>Login</button>
+            <h2 className={"nav-heading"}>InventoryApp</h2>
+            <Button>Login / Register</Button>
         </nav>
     )
 }
