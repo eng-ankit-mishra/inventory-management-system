6cbb3f7bd1245240576c38d14e3c798ffb800bf8
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2026-01-17 23:15:45 +0530

    Added header

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import Navbar from '../components/Navbar.jsx'
+
 export default function Home(){
     return(
         <section className="home">
             <Navbar />
-            <h1>Welcome to Inventory management app!</h1>
+            <main className="home-page">
+                <h1>Welcome to Inventory management app!</h1>
+            </main>
+
         </section>
 
     )
