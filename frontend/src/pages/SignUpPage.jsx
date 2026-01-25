b56f7f31f80753058b6e667b9d89e69dc5863c27
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2026-01-25 11:40:30 +0530

    Added sign up pages

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,23 @@
+import Navbar from "../components/Navbar.jsx";
+import Button from "../components/Button.jsx";
+
+export default function SignUpPage() {
+    return(
+        <section className="login-page">
+            <Navbar/>
+            <main className="login-card">
+                <h3>Inventory App</h3>
+                <h4>Welcome Back</h4>
+                <p>Please Sign in to your account</p>
+                <form className="login-form">
+                    <label>Email : </label>
+                    <input name="email" type="email" />
+                    <label>Password: </label>
+                    <input name="password" type="password" />
+                    <Button>Login</Button>
+                </form>
+                <a>Forget your password?</a>
+            </main>
+        </section>
+    )
+}
