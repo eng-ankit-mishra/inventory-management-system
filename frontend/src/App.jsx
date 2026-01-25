41c681ad464e09469d6c25079320cde146788520
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2026-01-25 11:35:24 +0530

    Added routes to different screens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
 import Home from './pages/Home.jsx'
 import './App.css'
+import {BrowserRouter,Route,Routes} from "react-router"
+import Dashboard from "./pages/Dashboard.jsx";
+import Product from "./pages/Product.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignupPage.jsx";
 
 export default function App(){
     return (
-        <Home/>
+        <BrowserRouter>
+            <Routes>
+               <Route path={"/"} element={<Home/>}/>
+                <Route path={"/dashboard"} element={<Dashboard />}/>
+                <Route path={"/products"} element={<Product/>}/>
+                <Route path={"/login"} element={<LoginPage/>}/>
+                <Route path={"/signup"} element={<SignUpPage/>}/>
+            </Routes>
+
+        </BrowserRouter>
+
     )
 }
