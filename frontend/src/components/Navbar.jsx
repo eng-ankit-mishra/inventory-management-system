cc38d4e984896647912bc3a632802053667ae7aa
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-01-28 23:44:19 +0530

    Added navigation to buttons

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import Button from './Button.jsx'
+import {Link, useNavigate} from "react-router-dom";
 export default function Navbar({AuthRequired=true}){
+    const navigate=useNavigate();
     return (
         <nav className="navbar">
-            <h2><a>InventoryApp</a></h2>
-            {AuthRequired && <Button>Login / Register</Button>}
+            <h2><Link to={"/"} className={"app-name"}>InventoryApp</Link></h2>
+            {AuthRequired && <Button onClick={()=>navigate("/login")}>Login / Register</Button>}
         </nav>
     )
 }
