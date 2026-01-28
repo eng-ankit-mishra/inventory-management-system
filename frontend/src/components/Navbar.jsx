d77d945305908c1e4065b5fcd0f1c1d7d6125b2d
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-01-28 18:35:53 +0530

    Made navbar responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import Button from './Button.jsx'
 export default function Navbar(){
     return (
         <nav className="navbar">
-            <h2 className={"nav-heading"}>InventoryApp</h2>
+            <h2><a>InventoryApp</a></h2>
             <Button>Login / Register</Button>
         </nav>
     )
