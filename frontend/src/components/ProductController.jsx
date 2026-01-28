09940609e3bd4574201ce76ab6ec3944a44575ab
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-01-28 19:58:55 +0530

    Added white spacing

--- frontend/src/components/ProductController.jsx
+++ frontend/src/components/ProductController.jsx
@@ -4,9 +4,19 @@ export default function ProductController() {
     return (
         <main className={"product-controller"}>
             <div className={"product-controller-selector"}>
-                <input placeholder={"Search by Name or SKU"}/>
-                <span>Filter by Category</span>
-                <span>Stock Status</span>
+                <input className={"search"} placeholder={"Search by Name or SKU"}/>
+                <select className={"category-selector"}>
+                    <option>Filter By Category</option>
+                    <option>Electronics</option>
+                    <option>TV</option>
+                    <option>Mobile</option>
+                </select>
+                <select className={"stock-selector"}>
+                    <option>Stock Status</option>
+                    <option>In Stock</option>
+                    <option>Low Stock</option>
+                    <option>Out of Stock</option>
+                </select>
             </div>
             <Button> + New Product</Button>
         </main>
