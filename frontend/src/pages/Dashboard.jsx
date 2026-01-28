f3541eed01777b2457802e0a92c2cfa17c63de7b
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-01-28 19:18:54 +0530

    Changed the background color

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,20 +6,23 @@ export default function Dashboard() {
         id:1,
         title: 'Total Products',
         description:'150',
+        color:"red"
     },
         {
             id:1,
             title: 'Total Value',
             description:'5,00,000',
+            color:"blue"
         },{
             id:1,
             title: 'Low Stock',
             description:'5',
+            color:"green"
         }]
 
     const productDetailsCard=productDetails.map((item)=>{
         return (
-            <div key={item.id} className={"product-cards"}>
+            <div key={item.id} className={`dashboard-card card-${item.color}`}>
                 <h3 className={"product-card-heading"}>{item.title}</h3>
                 <p className={"product-card-desc"}>{item.description}</p>
             </div>
