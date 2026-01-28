675ab278f10e7e01a9df98e1433dd0f449faa1da
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-01-28 19:18:38 +0530

    updated the font alignment

--- frontend/src/components/features.js
+++ frontend/src/components/features.js
@@ -2,17 +2,20 @@ export const featuresData = [
     {
         id: 1,
         title: "Real-time",
-        description: "Experience instant synchronization across all devices. Our system updates data live as it happens, ensuring you and your team are always viewing the most current information without manual refreshes."
+        description: "Experience instant synchronization across all devices. Our system updates data live as it happens, ensuring you and your team are always viewing the most current information without manual refreshes.",
+        color:"red"
     },
     {
         id: 2,
         title: "Secure-data",
-        description: "Your privacy is our priority. We employ end-to-end encryption and industry-standard security protocols to ensure your sensitive information remains protected against unauthorized access at all times."
+        description: "Your privacy is our priority. We employ end-to-end encryption and industry-standard security protocols to ensure your sensitive information remains protected against unauthorized access at all times.",
+        color:"blue"
     },
     {
         id: 3,
         title: "Analytics",
-        description: "Gain valuable insights with comprehensive dashboards. Visualize your performance metrics, track key trends, and make data-driven decisions to optimize your workflow and productivity."
+        description: "Gain valuable insights with comprehensive dashboards. Visualize your performance metrics, track key trends, and make data-driven decisions to optimize your workflow and productivity.",
+        color:"green"
     }
 ];
 
