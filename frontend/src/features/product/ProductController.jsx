b208a66e0951da734c4e1542ed09d59d505db947
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-02-01 19:30:26 +0530

    Added the logic for search and filter

--- frontend/src/features/product/ProductController.jsx
+++ frontend/src/features/product/ProductController.jsx
@@ -2,29 +2,29 @@ import Button from "../../components/common/Button.jsx";
 import {useState} from "react";
 import ProductModals from "./ProductModals.jsx";
 
-export default function ProductController() {
-    const [modals,setShowmodals]=useState(false)
+export default function ProductController({searchTerm,category,stockStatus,setSearchTerm,setStockStatus,setCategory}) {
+    const [modals,setShowModals]=useState(false);
 
     return (
         <main className={"product-controller"}>
             <div className={"product-controller-selector"}>
-                <input className={"search"} placeholder={"Search by Name or SKU"}/>
-                <select className={"category-selector"}>
-                    <option>Filter By Category</option>
-                    <option>Electronics</option>
-                    <option>TV</option>
-                    <option>Mobile</option>
+                <input value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} className={"search"} placeholder={"Search by Name or SKU"}/>
+                <select value={category} className={"category-selector"} onChange={(e)=>setCategory(e.target.value)}>
+                    <option value={"ALL"}>Filter By Category</option>
+                    <option value={"Refrigerator"}>Refrigerator</option>
+                    <option value={"TV"}>TV</option>
+                    <option value={"Smartphone"}>Smartphone</option>
                 </select>
-                <select className={"stock-selector"}>
-                    <option>Stock Status</option>
-                    <option>In Stock</option>
-                    <option>Low Stock</option>
-                    <option>Out of Stock</option>
+                <select value={stockStatus} className={"stock-selector"} onChange={(e)=>setStockStatus(e.target.value)}>
+                    <option value={"ALL"}>Stock Status</option>
+                    <option value={"IN_STOCK"}>In Stock</option>
+                    <option value={"LOW_STOCK"}>Low Stock</option>
+                    <option value={"OUT_OF_STOCK"}>Out of Stock</option>
                 </select>
             </div>
-            <Button onClick={()=>setShowmodals(true)}> + New Product</Button>
+            <Button onClick={()=>setShowModals(true)}> + New Product</Button>
             {modals && (
-                <ProductModals onClose={()=>setShowmodals(false)}/>
+                <ProductModals onClose={()=>setShowModals(false)}/>
             )}
         </main>
     )
