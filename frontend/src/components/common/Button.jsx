2c384ba8305cfcfc6edb3132965507b05ed2113a
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-02-01 19:32:31 +0530

    Added the type props

--- frontend/src/components/common/Button.jsx
+++ frontend/src/components/common/Button.jsx
@@ -1,6 +1,6 @@
-export default function Button({children,type="primary",onClick}){
+export default function Button({children,type="submit",style="primary",onClick}){
     return(
-        <button onClick={onClick} className={`btn-${type}`}>
+        <button onClick={onClick} type={type} className={`btn-${style}`}>
             {children}
         </button>
     )
