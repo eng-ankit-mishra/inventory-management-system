aa0b4ded0ef657ce0b591e7cb36d6734830c3885
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-02-09 00:44:58 +0530

    Updated the ui

--- frontend/src/features/auth/ForgetPassword.jsx
+++ frontend/src/features/auth/ForgetPassword.jsx
@@ -1,32 +1,53 @@
 import Button from "../../components/common/Button.jsx";
 import Navbar from "../../components/layout/Navbar.jsx";
 import {useNavigate} from "react-router-dom"
+import {useState} from "react";
 
 export default function ForgetPassword() {
+    const [msg,setMessage]=useState("");
     const navigate=useNavigate();
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        const email=e.target.email.value;
+
+        try{
+            const response=await fetch(`http://localhost:8080/api/auth/forgot-password?email=${email}`,{
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            })
+            if(response.ok){
+                console.log(response)
+                setMessage("A reset link has been sent.")
+            }else{
+                console.log(response)
+                setMessage("User not found")
+            }
+        }catch(err){
+            console.error("Error",err);
+        }
+
+    }
+
     return (
         <section className="forget-password">
-            <Navbar AuthRequired={false} />
+            <Navbar AuthRequired={true} />
             <main className={"forget-password-card"}>
                 <h4>Reset your password</h4>
-                <p>Please provide your email and a new password.</p>
-                <form className={"forget-password-form"}>
+                <p>Please provide your email.</p>
+                <form onSubmit={handleSubmit} className={"forget-password-form"}>
                     <label htmlFor="email">Email</label>
                     <input
                         id="email"
+                        name={"email"}
                         type="email"
                         placeholder="Enter your email"
                     />
-
-                    <label htmlFor="password">New Password</label>
-                    <input
-                        id="password"
-                        type="password"
-                        placeholder="Enter your new password"
-                    />
-
-                    <Button onClick={()=>navigate("/login")}>Submit</Button>
+                    <Button type={"submit"}>Send Reset Link</Button>
                 </form>
+                <p>{msg}</p>
             </main>
 
         </section>
