da7876685986b64d7abf891b6df15ddd1981581f
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-02-09 00:45:23 +0530

    Added the logic for get started buttons

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,9 +3,13 @@ import Footer from "../components/layout/Footer.jsx";
 import Button from "../components/common/Button.jsx";
 import {featuresData} from "../features/product/features.js"
 import {useNavigate} from "react-router-dom"
+import {useContext} from "react";
+import {AuthContext} from "../AuthContext/AuthProvider.jsx";
 
 export default function Home(){
     const navigate=useNavigate();
+
+    const {user}=useContext(AuthContext)
     const features=featuresData.map((feature)=>{
         return(
             <div key={feature.id} className={`cards card-${feature.color}`}>
@@ -14,6 +18,15 @@ export default function Home(){
             </div>
         )
     })
+
+    function handleButton(){
+        if(!user){
+            navigate("/login")
+        }else{
+            navigate("/dashboard")
+
+        }
+    }
     return(
         <section className="home">
             <Navbar />
@@ -22,7 +35,7 @@ export default function Home(){
                 <p>Manage Your Inventory with Ease
                     Track products, monitor stock, and analyze
                     data in one secure place.</p>
-                <Button onClick={()=>navigate("/login")} style={"secondary"}>Get Started</Button>
+                <Button onClick={handleButton} style={"secondary"}>Get Started</Button>
             </main>
             <main className="features-page">
                 <h2>Why choose us ?</h2>
