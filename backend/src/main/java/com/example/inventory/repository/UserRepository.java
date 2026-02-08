52b0a4490c66859afcf2b5a96ec832e71215ec2b
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-02-09 00:52:51 +0530

    Added new query to get user by email

--- backend/src/main/java/com/example/inventory/repository/UserRepository.java
+++ backend/src/main/java/com/example/inventory/repository/UserRepository.java
@@ -2,11 +2,24 @@ package com.example.inventory.repository;
 
 
 import com.example.inventory.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User a " +
+            "SET a.enabled = TRUE " +
+            "WHERE a.email = ?1")
+    int enableUser(String email);
+
+    List<User> findByEmailVerifiedTrueAndEnabledFalse();
 }
